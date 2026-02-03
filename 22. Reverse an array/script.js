b5f3d831b05e9d3a546935ee5556e7678e7edc4c
kuljeet-singhh <Kuljeet.pixlerlab@gmail.com>
2026-02-03 18:08:09 +0530

    initial commit

--- 22. Reverse an array/script.js
+++ 22. Reverse an array/script.js
@@ -1,9 +1,26 @@
+// let arr = [1, 2, 3, 4, 5, 6];
+// console.log("array = " +arr);
+// document.getElementById("demo1").innerHTML = "array = " + arr;
+// const sol = arr.reverse();
+
+
 let arr = [1, 2, 3, 4, 5, 6];
 console.log("array = " +arr);
 document.getElementById("demo1").innerHTML = "array = " + arr;
-const sol = arr.reverse();
-console.log("Reverse array = "+sol);
+let n= arr.length-1;
+let s=0;
+let e=n;
+let temp;
+
+while(s<e){
+    temp = arr[s];
+    arr[s] =arr[e];
+    arr[e] = temp;
+    s++;
+    e--;
+}
+
+console.log("Reverse array = "+arr);
 
 
-
-document.getElementById("demo").innerHTML = sol;
+document.getElementById("demo").innerHTML = arr;
--- 27/script.js
+++ 27/script.js
@@ -2,14 +2,25 @@
 
 function distinctEle(arr){
     let ans=[];
-for(let i=0;i<arr.length-1;i++){
-   ans.push(arr[i+1]-arr[i])
+for(let i=0;i<arr.length;i++){
+    let temp= false;
+    for(let j =0; j<ans.length;j++){
+       if(arr[i]===ans
+        [j]){
+        temp=true;
+      
+       }
+    }
+    if(!temp){
+        ans.push(arr[i])
+    }
+   
 }
 return ans;
 }
-let arr = [1,2,3,4,5]
+let arr = [2, 2, 3, 3, 7, 5,5]
 let ans = distinctEle(arr)
-console.log(`array = ${arr}`)
+console.log(`arrayyyy = ${arr}`)
 console.log(`distinct elements = ${ans}`)
 
 
@@ -17,3 +28,6 @@ console.log(`distinct elements = ${ans}`)
 
 document.getElementById("demo1").innerHTML=`array = ${arr}`;
 document.getElementById("demo2").innerHTML=`distinct elements = ${ans}`;
+
+
+
